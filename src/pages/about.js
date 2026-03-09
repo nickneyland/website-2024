04bd076277bf1a4ae6749d136957ea85132fe27a
Nick Neyland <nickneyland@gmail.com>
2026-03-09 13:43:23 -0400

    small change to bio

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,6 +7,7 @@ const aboutPage = () => (
         <div className="container">
             <p>I grew up on the south-east coast of England and now live in Brooklyn, NY.</p>
             <p>I currently work as the Vice President of Technology for <a href="https://www.islandgrid.com/" target="_blank" rel="noopener noreferrer">Island Grid</a> and <a href="https://www.bahamasgrid.net/" target="_blank" rel="noopener noreferrer">Bahamas Grid</a>.</p>
+            <p>I am also the co-founder and owner of <a href="https://thirdbranchconsulting.com/" target="_blank" rel="noopener noreferrer">Third Branch</a>.</p>
             <StaticImage
                 src="../images/nick.jpg" 
                 alt="photo of nick" 
